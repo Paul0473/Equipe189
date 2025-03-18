2e7f51ba5556c58d174bd7ad935fb8d4564342c3
Paul Bonneau Richard <pbric@ulaval.ca>
2025-03-18 15:42:22 -0400

    start

---
+++ newton.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+def newton(fonction_racine, derivee_fonction_racine, x0, tolr, nmax):
+    length_x0 = len(x0)
+    if length_x0 != 1:
+        iterations = np.array([x0]).reshape(length_x0,1)
+        prochaine_iteration = x0-np.linalg.solve(derivee_fonction_racine(x0),fonction_racine(x0))
+        iterations = np.append(iterations,prochaine_iteration.reshape(length_x0,1),1)
+        n = 1
+        eps = np.finfo(np.float64).eps
+        err_rel = np.linalg.norm(iterations[:,n]-iterations[:,n-1])/(np.linalg.norm(iterations[:,n])+eps)
+        while (err_rel >= tolr and n < nmax):
+            prochaine_iteration = iterations[:,n]-np.linalg.solve(derivee_fonction_racine(iterations[:,n]),
+                                                    fonction_racine(iterations[:,n]))
+            iterations = np.append(iterations,prochaine_iteration.reshape(length_x0,1),1)
+            err_rel = np.linalg.norm(iterations[:,n+1]-iterations[:,n])/(np.linalg.norm(iterations[:,n+1])+eps)
+            n += 1
+        return iterations
+    elif length_x0 == 1:
+        iterations = np.array([x0]).reshape(length_x0,1)
+        prochaine_iteration = x0-fonction_racine(x0)/derivee_fonction_racine(x0)
+        iterations = np.append(iterations,prochaine_iteration.reshape(length_x0,1),1)
+        n = 1
+        eps = np.finfo(np.float64).eps
+        err_rel = np.linalg.norm(iterations[:,n]-iterations[:,n-1])/(np.linalg.norm(iterations[:,n])+eps)
+        while (err_rel >= tolr and n < nmax):
+            prochaine_iteration = iterations[:,n]-fonction_racine(iterations[:,n])/derivee_fonction_racine(iterations[:,n])
+            iterations = np.append(iterations,prochaine_iteration.reshape(length_x0,1),1)
+            err_rel = np.linalg.norm(iterations[:,n+1]-iterations[:,n])/(np.linalg.norm(iterations[:,n+1])+eps)
+            n += 1
+        return iterations
